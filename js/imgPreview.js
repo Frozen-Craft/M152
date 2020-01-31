d410ccc92fc3b070464e6304481e1803e12f05b8
BuildTools <benzonana.a@gmail.com>
2020-01-31 19:20:50 +0100

    add comments

--- js/imgPreview.js
+++ js/imgPreview.js
@@ -1,3 +1,4 @@
+//Add small image under the textbox to show a preview of the selected images
 var loadFile = function(event) {
     var parent = document.getElementById("uploadedImg");
     parent.innerHTML="";
@@ -13,7 +14,7 @@ var loadFile = function(event) {
     }
 };
 
-
+//make an image bigger on clicking on it (for the preview images)
 function showBig(image){
     var parent = document.getElementById("bigSizeViewBack");
     parent.style.visibility = "visible";
@@ -21,10 +22,16 @@ function showBig(image){
     imgPrev.src = image;
 }
 
+//make the big image smaller on clicking anywhere
 function closeBigView(){
     document.getElementById("bigSizeViewBack").style.visibility = "hidden";
 }
 
+//make the big image smaller by hitting any key
 document.addEventListener('keyup', (e) => {
     closeBigView();
+    var key = e.key;
+    if(key != "Enter" && key != "Escape"){
+        document.getElementById("comment").focus();
+    }
 });
