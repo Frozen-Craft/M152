0e7da8ae36fcaf11a4faaa28dc4894fa009abc7a
BuildTools <benzonana.a@gmail.com>
2020-02-03 19:23:00 +0100

    class Post completion

--- model/class/Post.php
+++ model/class/Post.php
@@ -1,52 +1,118 @@
 <?php
 
-class Post {
+class Post
+{
 
-    private int $idPost;
-    private string $comment;
+    protected $idPost;
+    protected $comment;
 
-    private string $card;
+    protected $card;
 
-    function __construct($idPost, $comment){
-        
+    function __construct($idPost, $comment)
+    {
+        $this->$idPost = $idPost;
+        $this->$comment = $comment;
     }
 
-    function makeCard(){
-
+    function makeCard()
+    {
+        $card = '<div class="card mt-2 mb-2">';
+        $card .= $this->comment;
+        $card = '</div>';
+        $this->card = $card;
     }
 
-    function getCard(){
-
+    function getCard()
+    {
+        return $this->card;
     }
-    
+
 }
 
-class PostWithSingleImage extends Post{
+class PostWithSingleImage extends Post
+{
 
-    private array $pictures;
+    private $picture;
 
-    function __construct(){
-        parent::__construct();
+    function __construct($idPost, $comment, $picture)
+    {
+        parent::__construct($idPost, $comment);
+        $this->$picture = $picture;
     }
 
-    function makeCard(){
+    function makeCard()
+    {
+        $card = '<div class="card mt-2 mb-2">';
+        $card .= sprintf('<img src="%s" alt="%s"', $this->picture['path'], $this->picture['name']);
+        $card .= $this->comment;
+        $card .= '</div>';
+        $this->card = $card;
+    }
 
-    }    
+    function getCard()
+    {
+        parent::getCard();
+    }
 }
 
-class PostWithMultipleImage extends Post{
+class PostWithMultipleImage extends Post
+{
 
-    private array $pictures;
+    private $pictures;
 
-    function __construct(){
-        parent::__construct();
+    private $carousel;
+
+    function __construct($idPost, $comment, $pictures)
+    {
+        parent::__construct($idPost, $comment);
+        $this->$pictures = $pictures;
     }
 
-    function makeCard(){
-
+    function makeCard()
+    {
+        $this->makeCarousel();
     }
 
-    function makeCarousel(){
-        
+    function makeCarousel()
+    {
+        $carousel = '<div class="card mt-2 mb-2">
+                     <div id="carouselIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
+                     <ol class="carousel-indicators">';
+
+        for ($i = 0; $i < count($this->pictures); $i++) {
+            $carousel .= '<li data-target="#carouselIndicators" data-slide-to="' . $i . '" class="active"></li>';
+        }
+
+        $carousel .= '</ol>
+                      <div class="carousel-inner">';
+
+        foreach ($this->pictures as $p) {
+            $carousel .= '<div class="carousel-item">';
+            $carousel .= sprintf('<img src="%s" class="d-block w-100" alt="%s" />', $p['path'], $p['name']);
+            $carousel .= '</div>';
+        }
+        $carousel .= '</div>
+                      <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                      </a>
+                      </div>
+                      </div>';
+
+        $this->carousel = $carousel;
+    }
+
+    function getCard()
+    {
+        return $this->carousel;
+    }
+
+    function getCarousel()
+    {
+        return $this->carousel;
     }
 }
--- view/mainView.php
+++ view/mainView.php
@@ -3,62 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bootstrap.min.css" /> 
-    <link rel="stylesheet" href="css/style.css" /> 
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <title>Accueil</title>
 </head>
 <body>
-    <?php require("view/includes/nav.php") ?>
-    <main class="container">
-        <?= $info ?>
-        <div class="row mt-2 mx-auto">
-            <div class="col-md-4 mx-auto">
-                <div class="card mx-auto" >
-                    <img src="images/profilepicture.jpg" style="width: 90%;" class="card-img-top mt-2 mx-auto" alt="profilePicture">
-                    <div class="card-body">
-                        <p class="card-text">Ceci est ma photo de profile</p>
-                    </div>
+<?php require("view/includes/nav.php") ?>
+<main class="container">
+    <?= $info ?>
+    <div class="row mt-2 mb-2 mx-auto">
+        <div class="col-md-4 mx-auto">
+            <div class="card mx-auto" >
+                <img src="images/profilepicture.jpg" style="width: 90%;" class="card-img-top mt-2 mx-auto" alt="profilePicture">
+                <div class="card-body">
+                    <p class="card-text">Ceci est ma photo de profile</p>
                 </div>
             </div>
-            <div class="col-md-8 mx-auto">
-                <div class="card mx-auto">
-                    <div class="card-body">
-                        <h2 class="card-title">Welcome</h2>
-                    </div>
+        </div>
+        <div class="col-md-8 mx-auto">
+            <div class="card mx-auto">
+                <div class="card-body">
+                    <h2 class="card-title">Welcome</h2>
                 </div>
-<!-- TEST -->
-<div class="posts">
-    <div class="card mt-2 mb-2">
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="false">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="images/0e622f554aa48766a1f9cecfcc496adf.jpeg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/0e622f554aa48766a1f9cecfcc496adf.jpeg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/0e622f554aa48766a1f9cecfcc496adf.jpeg"  class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
-</div>
             </div>
-        </div class="mb-2">
-    </main>
+            <!-- TEST -->
+            <div class="posts">
+
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
